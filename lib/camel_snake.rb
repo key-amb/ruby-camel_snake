f8a55d53c153d328b57787428893aede4928f91e
key-amb <yasutake.kiyoshi@gmail.com>
2016-04-04 17:51:18 +0900

    Implement String conversion

--- lib/camel_snake.rb
+++ lib/camel_snake.rb
@@ -1,5 +1,19 @@
 require "camel_snake/version"
 
 module CamelSnake
-  # Your code goes here...
+  def to_pascal
+    gsub(/(?:\b|_)([a-z])/) { $1.upcase }
+  end
+
+  def to_camel firstletter=:capital
+    if firstletter == :capital
+      to_pascalcase
+    else
+      gsub(/_([a-z])/) { $1.upcase }
+    end
+  end
+
+  def to_snake
+    gsub(/\b([A-Z]+)/) { $1.downcase }.gsub(/(.)([A-Z]+)/, '\1_\2').downcase
+  end
 end
--- spec/camel_snake_spec.rb
+++ spec/camel_snake_spec.rb
@@ -1,11 +1,52 @@
 require 'spec_helper'
 
+class String
+  include CamelSnake
+end
+
 describe CamelSnake do
   it 'has a version number' do
     expect(CamelSnake::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#to_pascal' do
+    {
+      'hoge'        => 'Hoge',
+      'foo_bar1'    => 'FooBar1',
+      'a_b_c'       => 'ABC',
+      'foo_bar baz' => 'FooBar Baz',
+    }.each_pair do |sbj,expect|
+      it "#{sbj} => #{expect}" do
+        expect(sbj.to_pascal).to eq expect
+      end
+    end
+  end
+
+  describe '#to_camel' do
+    {
+      'hoge'        => 'hoge',
+      'foo_bar1'    => 'fooBar1',
+      'a_b_c'       => 'aBC',
+      'foo_bar baz' => 'fooBar baz',
+    }.each_pair do |sbj,expect|
+      it "#{sbj} => #{expect}" do
+        expect(sbj.to_camel :lower).to eq expect
+      end
+    end
+  end
+
+  describe '#to_snake' do
+    {
+      'ID'          => 'id',
+      'camelCase'   => 'camel_case',
+      'FooBar'      => 'foo_bar',
+      'ServiceID'   => 'service_id',
+      'HTTPRequest' => 'httprequest',
+      'FooBar ID'   => 'foo_bar id',
+    }.each_pair do |sbj,expect|
+      it "#{sbj} => #{expect}" do
+        expect(sbj.to_snake).to eq expect
+      end
+    end
   end
 end
